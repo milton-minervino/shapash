53be5ba5f74e87bee235199efd069f70c1b2e2d5
Milton Minervino <milton.minervino@inria.fr>
2026-06-25 15:58:01 +0200

    chore: add new objects in init

--- shapash/backend/__init__.py
+++ shapash/backend/__init__.py
@@ -3,6 +3,9 @@ import sys
 
 from .base_backend import BaseBackend
 from .lime_backend import LimeBackend
+from .nlp_backend import NlpBackend, NlpContributions, NlpRawExplanation
+from .nlp_lime_backend import NlpLimeBackend
+from .nlp_shap_backend import NlpShapBackend
 from .shap_backend import ShapBackend
 
 
